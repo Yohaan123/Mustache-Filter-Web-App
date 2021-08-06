513cada743a0564387163090175263e4b6d47f9e
Yohaan <80580077+Yohaan123@users.noreply.github.com>
2021-08-06 11:50:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function preload() {
-
+    mustache = loadImage("https://i.postimg.cc/44tZ4pzm/moustache.png");
 }
 
 function setup() {
@@ -18,16 +18,23 @@ function modelLoaded(){
 
 function draw() {
     image(video,0,0,300,300);
+    image(mustache,nosex,nosey,30,30);
 }
 
 function take_snapshot(){
     save("My_MustacheFilterImage.png");
 }
 
+nosex = 0;
+nosey = 0;
+
 function getPoses(results){
     if(results.length > 0){
         console.log(results);
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
+
+        nosex = results[0].pose.nose.x - 10;
+        nosey = results[0].pose.nose.y - 10;
     }
 }
